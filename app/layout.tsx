5305df0bf585ecd919e4558c894c0a64438b7cba
Guru Krithick M <gurukrithickm.ece2023@citchennai.net>
2025-02-01 10:35:05 +0530

    refactor: integrate Google Analytics and Vercel Speed Insights into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Poppins, Playfair_Display, Ephesis } from 'next/font/google'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Script from 'next/script'
 
 const poppins = Poppins({ 
   subsets: ['latin'],
@@ -29,11 +30,38 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const trackingId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${poppins.variable} ${playfair.variable} ${ephesis.variable} font-sans`}>
         {children}
+
+        {/* Vercel Speed Insights */}
         <SpeedInsights />
+
+        {/* Google Analytics */}
+        {trackingId && (
+          <>
+            <Script 
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`} 
+            />
+
+            <Script 
+              id="google-analytics" 
+              strategy="afterInteractive"
+            >
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${trackingId}');
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   )
